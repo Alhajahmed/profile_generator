43ad73d90b3730f66fbb7066d753ed4dec3e89d5
Ahmed <ahmed91alhaj@gmail.com>
2023-02-13 04:41:24 -0500

    funProfileGenerator

---
+++ survey.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("What's your name?", (answer) => {
+  console.log(`Thank you for your valuable feedback: ${answer}`);
+  rl.question("What's your name? Nicknames are also acceptable", (answer) => {
+    console.log(`Thank you for your valuable feedback: ${answer}`);
+    rl.question("What's an activity you like doing?", (answer) => {
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      rl.question("What do you listen to while doing that?", (answer) => {
+        console.log(`Thank you for your valuable feedback: ${answer}`);
+        rl.close();
+      });
+    });
+  });
+});
